0176a5a7f3754baab5345279050d0344712ddbba
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-01-11 20:14:25 +0700

    Update: ping command

--- src/Commands/ping.js
+++ src/Commands/ping.js
@@ -10,14 +10,19 @@ module.exports = new Command({
         beforeEmbed.setTitle('Pinging...');
         beforeEmbed.setColor('#FFFF01');
 
-        const afterEmbed = new Discord.MessageEmbed();
-        afterEmbed.setTitle('Pong!');
-        afterEmbed.setColor('#FFFF01');
-        afterEmbed.addField('Latency', `${message.createdTimestamp - Date.now()} ms.`, true)
-        afterEmbed.addField('API', `${Math.round(client.ws.ping)} ms`, true)
-
-
         const msg = await message.reply({ embeds: [beforeEmbed] })
-        msg.edit({ embeds: [afterEmbed] })
+
+        beforeEmbed.setTitle('Pinging.');
+        await new Promise(resolve => setTimeout(resolve, 125));
+        msg.edit({ embeds: [beforeEmbed] })
+        beforeEmbed.setTitle('Pinging..');
+        await new Promise(resolve => setTimeout(resolve, 125));
+        msg.edit({ embeds: [beforeEmbed] })
+        beforeEmbed.setTitle('Pinging...');
+        await new Promise(resolve => setTimeout(resolve, 125));
+        msg.edit({ embeds: [beforeEmbed] })
+        beforeEmbed.setTitle(`Pong! ${Math.round(client.ws.ping)} ms`);
+        await new Promise(resolve => setTimeout(resolve, 125));
+        msg.edit({ embeds: [beforeEmbed] })
     }
 });
