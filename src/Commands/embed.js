9e7d36dbd8f069a56b9cc4bf76bc8feef2b87f35
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-01-11 14:43:27 +0700

    Add: Embed command

---
+++ src/Commands/embed.js
@@ -0,0 +1,16 @@
+const Command = require('../Structures/Command.js');
+const Discord = require('discord.js');
+
+module.exports = new Command({
+    name: 'embed',
+    description: 'Show an embed',
+    
+    async run(message, args, client) {
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle('This is a test embed title');
+        embed.setDescription('This is a test embed description');
+        embed.setColor('#FFFF01');
+
+        message.reply({ embeds: [embed] })
+    }
+});
